b27aef32b270c152f9a0a7dceb512cfa24bd5931
linoor <2622530+linoor@users.noreply.github.com>
2024-04-05 17:44:56 +0200

    added test for k before front vowels

--- tests/cyrillic-neo-alif.test.ts
+++ tests/cyrillic-neo-alif.test.ts
@@ -4,12 +4,20 @@ import { translate } from "../src/ohm"
 
 describe('cyrillic-neo-alif', () => {
     test('correctly parse к in front of back vowels', () => {
-        expect(translate('ка')).toBe('qa')
-        expect(translate('кa')).toBe('qa')
+        expect(translate('ка')).toBe('qa') // russian a
+        expect(translate('кa')).toBe('qa') // latin a
         expect(translate('ко')).toBe('qo')
-        expect(translate('ку')).toBe('qu')
-        expect(translate('кy')).toBe('qu')
+        expect(translate('ку')).toBe('qu') // russian y
+        expect(translate('кy')).toBe('qu') // latin y
         expect(translate('кы')).toBe('qı')
         expect(translate('кый')).toBe('qıy')
+    }),
+    test('correctly parse к in front of front vowels', () => {
+        expect(translate('ке')).toBe('ke') // russian e
+        expect(translate('кe')).toBe('ke') // latin e
+        expect(translate('кә')).toBe('kə') 
+        expect(translate('кө')).toBe('kö')
+        expect(translate('кү')).toBe('kü')
+        expect(translate('кэ')).toBe('ke')
     })
 })
