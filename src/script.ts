1a83906c470bba5dd1499f4183849412073d8d45
linoor <2622530+linoor@users.noreply.github.com>
2024-04-05 17:44:46 +0200

    working g with front and back vowels

--- src/script.ts
+++ src/script.ts
@@ -3,48 +3,31 @@ import * as ohm from 'ohm-js';
 const mapping: { [key: string]: { [key: string]: { [key: string]: string } } } = {
     'Cyrillic': {
         'NeoAlif': {
-            'А': 'A', 'а': 'a',
-            'Ә': 'Ə', 'ә': 'ə',
-            'Б': 'B', 'б': 'b',
-            'Җ': 'C', 'җ': 'c',
-            'Ч': 'Ç', 'ч': 'ç',
-            'Д': 'D', 'д': 'd',
-            'Э': 'E', 'э': 'e',
-            'Ф': 'F', 'ф': 'f',
-            'Г': 'G', 'г': 'g',
-            'Һ': 'H', 'һ': 'h',
-            'Ы': 'I', 'ы': 'ı',
-            'И': 'İ', 'и': 'i',
-            'Ж': 'J', 'ж': 'j',
-            'К': 'K', 'к': 'k',
-            'Л': 'L', 'л': 'l',
-            'М': 'M', 'м': 'm',
-            'Н': 'N', 'н': 'n',
-            'Ң': 'Ñ', 'ң': 'ñ',
-            'О': 'O', 'о': 'o',
-            'Ө': 'Ö', 'ө': 'ö',
-            'П': 'P', 'п': 'p',
-            'Р': 'R', 'р': 'r',
-            'С': 'S', 'с': 's',
-            'Ш': 'Ş', 'ш': 'ş',
-            'Т': 'T', 'т': 't',
-            'У': 'U', 'у': 'u',
-            'Ү': 'Ü', 'ү': 'ü',
-            'В': 'V', 'в': 'v',
-            'Х': 'X', 'х': 'x',
-            'Й': 'Y', 'й': 'y',
-            'З': 'Z', 'з': 'z',
-            'ь': "'",
-            'я': 'ya', 'Я': 'Ya',
+            'а': 'a', 'ә': 'ə', 'б': 'b',
+            "a": "a", "e": "e", "o": "o", "y": "y",
+            'җ': 'c', 'ч': 'ç', 'д': 'd',
+            'э': 'e', 'ф': 'f', "г": "g",
+            'һ': 'h', 'ы': 'ı', 'и': 'i',
+            'ж': 'j', 'к': 'k', 'л': 'l',
+            'м': 'm', 'н': 'n', 'ң': 'ñ',
+            'о': 'o', 'ө': 'ö', 'п': 'p',
+            'р': 'r', 'с': 's', 'ш': 'ş',
+            'т': 't', 'у': 'u', 'ү': 'ü',
+            'в': 'v', 'х': 'x', 'й': 'y',
+            'з': 'z', 'ь': "'", 'я': 'ya',
             'е': 'e', 'Е': 'e',
+            "g_back_vowel": "ğ", "g_front_vowel": "g"
         }
     }
 };
 const g = ohm.grammar(`Cyrillic {
-    input = (cyrillic_letter | any)*
-    cyrillic_letter = uppercaseLetter | lowercaseLetter
-    uppercaseLetter = "А" | "Ә" | "Б" | "Җ" | "Ч" | "Д" | "Э" | "Ф" | "Г" | "Һ" | "Ы" | "И" | "Ж" | "К" | "Л" | "М" | "Н" | "Ң" | "О" | "Ө" | "П" | "Р" | "С" | "Ш" | "Т" | "У" | "Ү" | "В" | "Х" | "Й" | "З" | "Ъ" | "Э" | "E" | "Я"
-    lowercaseLetter = "а" | "ә" | "б" | "җ" | "ч" | "д" | "э" | "ф" | "г" | "һ" | "ы" | "и" | "ж" | "к" | "л" | "м" | "н" | "ң" | "о" | "ө" | "п" | "р" | "с" | "ш" | "т" | "у" | "ү" | "в" | "х" | "й" | "з" | "ь" | "э" | "е" | "я"
+    input = (syllable | cyrillic_letter | any)*
+    cyrillic_letter = (consonant | front_vowel | back_vowel)
+    syllable = "г" &front_vowel -- g_front_vowel
+                | "г" &back_vowel -- g_back_vowel
+    consonant = "б" | "в" | "д" | "ж" | "г" | "з" | "й" | "к" | "л" | "м" | "н" | "ң" | "п" | "р" | "с" | "т" | "ф" | "х" | "һ" | "ч" | "ш" | "җ" | "ц" | "щ"
+    front_vowel = "ә" | "ө" | "ү" | "е" | "e" | "э"
+    back_vowel = "а" | "a" | "о" | "o" | "у" | "y" | "ы" | "и" | "ый"
 }`)
 
 const semantics = g.createSemantics().addOperation('translate', {
@@ -54,10 +37,24 @@ const semantics = g.createSemantics().addOperation('translate', {
     input(ps) {
         return ps.children.map(c => c.translate()).join('');
     },
+    syllable_g_front_vowel(_syllable, _vowel) {
+        return getEquivalent("g_front_vowel")
+    },
+    syllable_g_back_vowel(_syllable, _vowel) {
+        return getEquivalent("g_back_vowel")
+    },
     cyrillic_letter(ps) {
-        return ps.children.map(c => mapping['Cyrillic']['NeoAlif'][c.sourceString] || '?').join('');
+        return ps.children.map(c => getEquivalent(c.sourceString)).join('');
     },
 })
+
+function getEquivalent(c: string): string {
+    let equivalent = mapping['Cyrillic']['NeoAlif'][c] || '?';
+
+    return c === equivalent.toUpperCase() ?
+        equivalent.toUpperCase() :
+        equivalent;
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const inputArea = document.getElementById('inputArea') as HTMLTextAreaElement;
