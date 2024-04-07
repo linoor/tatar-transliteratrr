e05db9ba5bf0d51422bedad56dc7ceebf5e3052d
Michał Pomarański <2622530+linoor@users.noreply.github.com>
2024-04-07 21:00:42 +0200

    fixed 'i' that should be in front vowels

--- tests/cyrillic-neo-alif.test.ts
+++ tests/cyrillic-neo-alif.test.ts
@@ -10,8 +10,6 @@ describe('cyrillic-neo-alif', () => {
         expect(translate('ку')).toBe('qu') // russian y
         expect(translate('кy')).toBe('qu') // latin y
         expect(translate('кы')).toBe('qı')
-        expect(translate('ки')).toBe('qi')
-        expect(translate('кый')).toBe('qıy')
     }),
     test('correctly parse к in front vowels', () => {
         expect(translate('ке')).toBe('ke') // russian e
@@ -20,6 +18,7 @@ describe('cyrillic-neo-alif', () => {
         expect(translate('кө')).toBe('kö')
         expect(translate('кү')).toBe('kü')
         expect(translate('кэ')).toBe('ke')
+        expect(translate('ки')).toBe('ki')
     })
     test('correctly parse г in back vowels', () => {
         expect(translate('га')).toBe('ğa') // russian a
@@ -27,9 +26,7 @@ describe('cyrillic-neo-alif', () => {
         expect(translate('го')).toBe('ğo')
         expect(translate('гу')).toBe('ğu') // russian y
         expect(translate('гy')).toBe('ğu') // latin y
-        expect(translate('ги')).toBe('ği')
         expect(translate('гы')).toBe('ğı')
-        expect(translate('гый')).toBe('ğıy')
     }),
     test('correctly parse г in front vowels', () => {
         expect(translate('ге')).toBe('ge') // russian e
@@ -38,6 +35,7 @@ describe('cyrillic-neo-alif', () => {
         expect(translate('гө')).toBe('gö')
         expect(translate('гү')).toBe('gü')
         expect(translate('гэ')).toBe('ge')
+        expect(translate('ги')).toBe('gi')
     }),
     test('correctly parse "e" after consonants', () => {
         expect(translate('ке')).toBe('ke')
@@ -63,17 +61,6 @@ describe('cyrillic-neo-alif', () => {
         expect(translate('це')).toBe('tse')
         expect(translate('ще')).toBe('şçe')
     })
-    test('correctly parse "e" after back vowels', () => {
-        expect(translate('га')).toBe('ğa') // russian a
-        expect(translate('гa')).toBe('ğa') // latin a
-        expect(translate('го')).toBe('ğo')
-        expect(translate('гу')).toBe('ğu') // russian y
-        expect(translate('гy')).toBe('ğu') // latin y
-        expect(translate('ги')).toBe('ği')
-        expect(translate('гы')).toBe('ğı')
-        expect(translate('гый')).toBe('ğıy')
-    })
-
     test('correctly parse "ё" in russian loanwords', () => {
         expect(translate('гё')).toBe('ğyo') // russian г
         expect(translate('гё')).toBe('ğyo') // latin г
